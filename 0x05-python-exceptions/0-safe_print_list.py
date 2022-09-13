4d29891a7ee78212b1cbc97ed526689157939e56
Opemipo <104791614+Ope22@users.noreply.github.com>
2022-09-12 22:29:18 -0300

    Update 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    a = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[a]), end="")
+            a += 1
+        except Exception:
+            None
+    print()
+    return (a)
